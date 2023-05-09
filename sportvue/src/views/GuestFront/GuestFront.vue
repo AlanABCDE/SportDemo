77230b59bdbe4347057345afd5dce9b3daf5482b
AlanABCDE <1343700327@qq.com>
2023-05-10 00:25:38 +0800

    添加页面

---
+++ sportvue/src/views/GuestFront/GuestFront.vue
@@ -0,0 +1,150 @@
+<template>
+    <div class="common-layout">
+        <el-container>
+            <el-container>
+                <div class="head">
+                    <el-menu default-active="1" class="menu" mode="horizontal" @select="handleSelect">
+                        <el-menu-item index="1">
+                            <RouterLink to="guesthome">
+                                <h1 class="h1">主页</h1>
+                            </RouterLink>
+                        </el-menu-item>
+                        <el-tooltip class="box-item" effect="dark" content="登录/注册 后可访问" placement="bottom">
+                            <el-menu-item index="2" disabled>
+                                <RouterLink to="holdevent">
+                                    <h1 class="h1">举办赛事</h1>
+                                </RouterLink>
+                            </el-menu-item>
+                        </el-tooltip>
+                        <el-tooltip class="box-item" effect="dark" content="登录/注册 后可访问" placement="bottom">
+                            <el-menu-item index="3" disabled>
+                                <RouterLink to="managemyevent">
+                                    <h1 class="h1">管理我的赛事</h1>
+                                </RouterLink>
+                            </el-menu-item>
+                        </el-tooltip>
+                        <el-menu-item index="4">
+                            <RouterLink to="joinevent">
+                                <h1 class="h1">寻找赛事</h1>
+                            </RouterLink>
+                        </el-menu-item>
+                        <el-tooltip class="box-item" effect="dark" content="登录/注册 后可访问" placement="bottom">
+                            <el-menu-item index="5" disabled>
+                                <RouterLink to="team">
+                                    <h1 class="h1">小队查询</h1>
+                                </RouterLink>
+                            </el-menu-item>
+                        </el-tooltip>
+                        <el-tooltip class="box-item" effect="dark" content="登录/注册 后可访问" placement="bottom">
+                            <el-menu-item index="6" disabled>
+                                <RouterLink to="usermsg">
+                                    <h1 class="h1">通知</h1>
+                                </RouterLink>
+                            </el-menu-item>
+                        </el-tooltip>
+                        <el-menu-item index="7">
+                            <RouterLink to="scoreboard">
+                                <h1 class="h1">得分榜</h1>
+                            </RouterLink>
+                        </el-menu-item>
+                        <el-tooltip class="box-item" effect="dark" content="登录/注册 后可访问" placement="bottom">
+                            <el-sub-menu index="8" disabled>
+                                <template #title>
+                                    <h1 class="h1">个人中心</h1>
+                                </template>
+                                <el-menu-item index="8-1" disabled>
+                                    <RouterLink to="personalmainpage">
+                                        <h1 class="h1">个人主页</h1>
+                                    </RouterLink>
+                                </el-menu-item>
+                                <el-menu-item index="8-2" disabled>
+                                    <RouterLink to="personalavatar">
+                                        <h1 class="h1">修改头像</h1>
+                                    </RouterLink>
+                                </el-menu-item>
+                            </el-sub-menu>
+                        </el-tooltip>
+                        <el-menu-item index="9">
+                            <RouterLink to="/">
+                                <h1 class="h1">门户</h1>
+                            </RouterLink>
+                        </el-menu-item>
+                        <el-sub-menu>
+                            <template #title>
+                                <h1 class="h1">有事联系我</h1>
+                            </template>
+                            <img src="https://raw.githubusercontent.com/AlanABCDE/SportDemo/main/sportvue/src/assets/QRCode.png"
+                                class="erweima" />
+                        </el-sub-menu>
+                        <div class="User-Avatar">
+                            <span>你好，访客。</span>
+                            <el-button round @click="log">注册/登录</el-button>
+                        </div>
+                    </el-menu>
+                </div>
+                <el-main class="box"><router-view></router-view></el-main>
+
+            </el-container>
+        </el-container>
+    </div>
+</template>
+<script >
+export default {
+    name: "Front",
+    data() {
+        return {
+            user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        log() {
+            this.$router.push('/login');
+        }
+    }
+}
+</script>
+<style>
+.el-aside {
+    width: 200px;
+}
+
+.common-layout {
+    /* background-image:url('https://d34ymitoc1pg7m.cloudfront.net/bf3/backgrounds/bg-bf3-premium-3b9d9943.jpg');
+    height: 100%; */
+}
+
+.head {
+    background-image: url('E:\Code\Driving\SportDemo\sportvue\src\assets\backlogo.png');
+    width: 100%;
+    position: absolute;
+    height: 65px
+}
+
+.menu {
+    background-color: rgb(236, 241, 236, 0.8);
+    color: rgb(0, 0, 0);
+    font-weight: 700;
+}
+
+.h1 {
+    font-size: 20px;
+    font-weight: 700;
+}
+
+.erweima {
+    width: 120px;
+    margin: 0 20%;
+}
+
+.User-Avatar {
+    margin: 0.8% 0 0 25%;
+}
+
+.box {
+    background-color: rgb(229, 248, 242);
+    height: 1080px;
+}
+</style>
---
+++ sportvue/src/views/GuestFront/GuestHome.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="box">
+    <h1>欢迎使用校园体育赛事管理网站</h1>
+    <div>
+        <div class="demo-image__lazy">
+            <el-carousel indicator-position="outside">
+                <el-carousel-item v-for="url in urls" :key="url">
+                    <img :src="url" class="el-carousel-item-img" />
+                </el-carousel-item>
+            </el-carousel>
+        </div>
+
+        <div style="margin:auto;width:90%">
+            <el-row :gutter="12">
+                <el-col :span="8">
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>新闻</span>
+                            <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+                        </div>
+                        <div class="text item">
+                            <div style="width:200px">
+                                <el-link href="https://element.eleme.io" target="_blank">喜迎二十大</el-link>
+                                <el-link href="https://element.eleme.io" target="_blank">恭喜阿根廷世界杯夺冠</el-link>
+                                <el-link href="https://element.eleme.io" target="_blank">奋力开创体育强国建设新局面</el-link>
+                                <el-link href="https://element.eleme.io" target="_blank">少年强则国强</el-link>
+                            </div>
+                        </div>
+                    </el-card>
+                </el-col>
+                <el-col :span="8">
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>活动</span>
+                            <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+                        </div>
+                        <div class="text item">
+                            <div style="width:200px">
+                                <el-link   href="https://element.eleme.io" target="_blank">基沃托斯光轮大赛正式开幕</el-link>
+                                <el-link   href="https://element.eleme.io" target="_blank">FIFA World Cup大赛正式开幕</el-link>
+                                <el-link   href="https://element.eleme.io" target="_blank">CBA正式开幕</el-link>
+                                <el-link   href="https://element.eleme.io" target="_blank">NBA季后赛赛正式开幕</el-link>
+                            </div>
+                        </div>
+                    </el-card>
+                </el-col>
+                <el-col :span="8">
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>公告</span>
+                            <el-button style="float: right; padding: 3px 0" type="text">更多</el-button>
+                        </div>
+                        <div class="text item">
+                            <div style="width:200px">
+                                <el-link href="https://element.eleme.io" target="_blank">关于平台开始试运行的一则公告</el-link>
+                                <el-link href="https://element.eleme.io" target="_blank">关于平台现存漏洞的一则公告</el-link>
+                                <el-link href="https://element.eleme.io" target="_blank">关于平台开始维护的一则公告</el-link>
+                                <el-link href="https://element.eleme.io" target="_blank">关于平台的版权声明的一则公告</el-link>
+                            </div>
+                        </div>
+                    </el-card>
+                </el-col>
+            </el-row>
+        </div>
+
+    </div>
+</div>
+</template>
+<script lang="ts" setup>
+
+const urls = [
+    'https://cdn4.eyeem.com/thumb/b62716d62756471677a3a3a313236323031363837363634313d223235346332356336316031616669366338313039356236653332693163343935373163343661653a3a3a3/1100/1100',
+    'https://ts1.cn.mm.bing.net/th/id/R-C.010f83b74aa0e87c53bac1e885fa8434?rik=ugeClDWr3GRdeg&riu=http%3a%2f%2fpic.zsucai.com%2ffiles%2f2013%2f0718%2ftycx8.jpg&ehk=suCZTMpGpy01JZCjblUW%2btsTQC0p6xeE8%2ftRKmvdTBI%3d&risl=&pid=ImgRaw&r=0',
+    'https://png.pngtree.com/back_origin_pic/04/04/20/af56207b880b599795a67ef59c434c26.jpg',
+    'https://cdn4.eyeem.com/thumb/b62716d62756471677a3a3a353132323238323033303835313d283232393234356262323231633832326638333666353165336260383331373135643430303466616a3a3a3/1100/1100',
+]
+</script>
+<style>
+.text {
+    font-size: 14px;
+}
+
+.item {
+    margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+    display: table;
+    content: "";
+}
+
+.clearfix:after {
+    clear: both
+}
+
+.box-card {
+    width: 450px;
+}
+
+.el-card__body {
+    width: 100px;
+}
+
+.el-carousel-item-img {
+    height: 100%;
+}
+
+</style>
